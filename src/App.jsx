f445893f3090e12ca2fe0752d1cd97988a09415d
Amber Adamson <amber.renee.adamson@gmail.com>
2025-04-10 22:39:02 -0400

    Keyboard is fully styled. Just need to fix some things. Also added light/dark mode toggle.

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,69 @@ import data from './data.json'
 
 function App() {
 
+  // map over the array of objects and extract the values for the key of "word"
+  // pass the value from the key value pair into a variable
   const extractedWords = data.map(element => element.word);
-  // console.log(extractedWords)
 
+  // function to identify a random word from extractedWords
   const randomArrayWord = () => {
     const randomIndex = Math.floor(Math.random() * extractedWords.length);
     return extractedWords[randomIndex];
   }
 
-  // Create a state for the word the user guesses
+  // Create states: the default word, a user's guessed word, the array of guesses, index of a guessed word
   const [currentWord, setCurrentWord] = useState(randomArrayWord(""))
-  // Create another state for the word the user guesses
   const [guessedWord, setGuessedWord] = useState([])
-  // Create a state for all guesses (an array of arrays)
   const [allGuesses, setAllGuesses] = useState([])
-  // Create a state variable for the index of a guessed word
   const [currentRowIndex, setCurrentRowIndex] = useState(0)
-  // Create a state variable for the UI to shake when a word is invalid
+
+  // Create states: index of row for shake effect, toastMessage for conditional rendering of toast
   const [shakeRowIndex, setShakeRowIndex] = useState(null);
-  // Create a state variable for the toast
   const [toastMessage, setToastMessage] = useState(false)
-  // Create a state variable to compare guessed letters to those of the held word
+
+  // Create states: letterStatuses array to track correct, present, absent, classNames to style CSS class names based on letterStatuses for board
   const [letterStatuses, setLetterStatuses] = useState([])
-  // Create a state array for the class names
   const [classNames, setClassNames] = useState([])
-  // Create a state for a win message
+
+  // Create states: gameWon and gameLoss for conditional rendering of win/loss messages
   const [gameWon, setGameWon] = useState(false)
-  // Create another state for a loss message
   const [gameLoss, setGameLoss] = useState(false)
-  // Create a state object for styling the keyboard
+
+  // Create state: keyStatuses to style the keyboard keys based on guessed letter styling and disabled letters for letters not in the word
   const [keyStatuses, setKeyStatuses] = useState({})
-  // Create a state for disabled letters for the keyboard styling
   const [disabledLetters, setDisabledLetters] = useState([])
 
-  //useEffect hook to prevent infinite re-renderings of the currentWord value
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+    // Store the preference in local storage
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  useEffect(() => {
+    // Retrieve the preference from local storage on component mount
+    const storedDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+    if (storedDarkMode !== null) {
+      setDarkMode(storedDarkMode);
+    }
+  }, []);
+
+  // Hold the currentWord when the component mounts to prevent re-rendering of the default word
+  // Also normalize the default word with uppercase letters
   useEffect(() => {
     const newWord = randomArrayWord().toUpperCase(); // normalize here
     console.log("New random word set inside useEffect:", newWord);
     setCurrentWord(newWord);
   }, []);
+
+  const handleToggle = () => {
+    setDarkMode(!darkMode);
+  };
 
  
 
@@ -51,18 +75,12 @@ function App() {
   // Create a blank array with the same letter count as the current word
   const wordRow = currentWordArray.map((letter) => ('')); 
 
-  const keyboardRows = [
-    "QWERTYUIOP".split(""),
-    "ASDFGHJKL".split(""),
-    ["Enter", ..."ZXCVBNM".split(""), "Delete"]
-  ];
-
-
-// Create a function to hold the guessed letters in state
+// Function for a user to add letters to the board and guess a word
 const guessWord = (letter) => {
-      // Convert to lowercase for comparison
+      // Pass in "letter" from the keyboard map
   if (letter === "Delete" && !gameWon) {
     setGuessedWord(prev => prev.slice(0, -1));
+    // Slice method to delete letters
   } else if (letter === "Enter") {
     const guessedWordStr = guessedWord.join('');
     const isValidWord = extractedWords.map(word => word.toLowerCase()).includes(guessedWordStr.toLowerCase());
@@ -70,6 +88,7 @@ const guessWord = (letter) => {
     console.log(`guessedWord: "${guessedWord}" | currentWord: "${currentWord}"`);
     if (guessedWordStr.length !== currentWord.length) {
       triggerShakeEffect();
+      // If the guessed word is too short, trigger a shake effect
     } else if (guessedWordStr === currentWord) {
       console.log(`Winning condition met! guessedWord: ${guessedWordStr}, currentWord: ${currentWord}`);
       triggerWin()
@@ -89,6 +108,7 @@ const guessWord = (letter) => {
       }
   }
 
+  // Function to add the guess to the array of guesses, update the row index, and initialize a new array for a new guess
 const addtoGuessandReset = () => {
   setAllGuesses(prevGuesses => {
     let newGuesses = [...prevGuesses];
@@ -109,6 +129,7 @@ const addtoGuessandReset = () => {
   setGuessedWord([]);
   console.log("After clearing guessedWord:", guessedWord); // This won't reflect immediately
 };
+
 
 const addStatusesandClasses = () => {
   let newStatuses = [...letterStatuses]; // Copy the existing state
@@ -220,10 +241,14 @@ useEffect(() => {
   setAllGuesses([...allGuesses, ...missingRows]);
 }, [currentWord.length]); // Dependency array to trigger when state changes
 
-
+const keyboardRows = [
+  "QWERTYUIOP".split(""),
+  "ASDFGHJKL".split(""),
+  ["Enter", ..."ZXCVBNM".split(""), "Delete"]
+];
     // keyboard elements: nested map to iterate over elements in nested arrays
     const keyboardElements = keyboardRows.map((row, rowIndex) => (
-      <div key={rowIndex}>{row.map((letter, letterIndex) => {
+      <div key={rowIndex} className="keyboard-row">{row.map((letter, letterIndex) => {
         // class for letter styling
         const buttonClass = keyStatuses[letter] == "correct" ? "bg-green" : 
         keyStatuses[letter] == "present" ? "bg-yellow" : 
@@ -244,12 +269,24 @@ useEffect(() => {
         letterIndex === 9 ? "group-9-class" :
         letterIndex === 10 ? "group-10-class" : null
 
-        const combinedClass = `${gradientClass} ${buttonClass}`.trim();
-      
+        const rainbowLetter = letterIndex === 0 ? "letter-0-class" :
+        letterIndex === 1 ? "letter-1-class" :
+        letterIndex === 2 ? "letter-2-class" :
+        letterIndex === 3 ? "letter-3-class" :
+        letterIndex === 4 ? "letter-4-class" :
+        letterIndex === 5 ? "letter-5-class" :
+        letterIndex === 6 ? "letter-6-class" : 
+        letterIndex === 7 ? "letter-7-class" :
+        letterIndex === 8 ? "letter-8-class" :
+        letterIndex === 9 ? "letter-9-class" :
+        letterIndex === 10 ? "letter-10-class" : null
+
+        // going to need to fix this because dark mode is currently overriding the styling of the guessed letters
+        const combinedClass = `${gradientClass} ${buttonClass} ${darkMode ? 'dark-mode' : ''}`.trim();
 
         return (
         <button key={letterIndex} className={combinedClass} onClick={() => {guessWord(letter)}} disabled={disabledLetters.includes(letter)}
->{letter}</button>
+><span className={rainbowLetter}>{letter}</span></button>
       );
 })}
         </div>
@@ -277,7 +314,14 @@ const showToast = () => {
 
 
   return (
-    <>
+    <main className={`${darkMode ? 'dark-mode' : ''}`}>
+    <nav className="flex justify-end items-center p-2">
+  <label className="switch">
+    <input type="checkbox" onClick={handleToggle} />
+    <span className="slider round"></span>
+  </label>
+</nav>
+
     {toastMessage && <div className="fixed top-5 left-1/2 -translate-x-1/2 bg-indigo-500 text-white px-4 py-2 rounded-md shadow-lg transition-opacity duration-500">Not in word list!</div>}
     {/* Toast for words not in the list */}
 {/* Title For Application Goes Here */}
@@ -314,7 +358,7 @@ const showToast = () => {
 <section className="text-center mt-16">
                 {keyboardElements}
 </section>
-</>
+</main>
   )
 }
 
