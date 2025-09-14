7c7bab587edc146ee1464031e85d29bf6c5c7540
Amber Adamson <amber.renee.adamson@gmail.com>
2025-09-14 14:12:38 -0400

    Trying another fix: Specific iPad breakpoint.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header({ handleToggle, toastMessage, darkMode }) {
 
       {/* Title For Application*/}
       <div className="flex justify-center mt-16 sm:mb-12">
-        <div className="w-full sm:max-w-[690px] md:px-4">
+        <div className="w-full sm:max-w-[690px] md:px-4 terminal-container">
           <h1 className="sm:text-3xl font-bold text-lg pl-[50px] sm:pl-0 md:pl-0">
             <span className="terminal-title ipad:w-[23ch]">
               🚀Hashle: An Evolving Word Game
